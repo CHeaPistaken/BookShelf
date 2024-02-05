f4d319ada3fd3aa7256415262e96ea16676d5337
CHeaPistaken <92684766+CHeaPistaken@users.noreply.github.com>
2024-02-05 09:23:11 -0500

    Create BookShelf.cpp
    awoqeiuiaw8y

---
+++ BookShelf.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+  cout << "Enter the bookshelf dimensions";
+  cin >> x, y;
+  
+  vector<string> posarray{};
